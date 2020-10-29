e16bafae538a0e38666e4e5fdd86047fc4565b15
Erick Suehiro <ericksuehiro@gmail.com>
2020-10-29 01:32:10 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
-
+const { v4: uuid, validate: isUuid } = require('uuid');
 const app = express();
 
 app.use(express.json());
@@ -11,23 +9,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  const repositoryindex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryindex < 0){
+    return response.status(400).send();
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryindex].likes,
+  };
+
+  repositories[repositoryindex] = repository
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryindex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryindex < 0){
+    return response.status(400).send();
+  }
+
+  repositories.splice(repositoryindex, 1);
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const {id} = request.params;
 
+  const repositoryindex = repositories.findIndex(repository => repository.id == id);
+  const repository = repositories.find(repository => repository.id == id);
+
+  if (repositoryindex < 0){
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
 module.exports = app;
